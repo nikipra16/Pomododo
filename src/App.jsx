084ba8825e218eb1ada3eff2bda14f1202e84b5f
nikipra16 <142063028+nikipra16@users.noreply.github.com>
2024-10-30 21:59:00 -0700

    Added break timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import './index.css'
 import { useState, useEffect } from 'react';
@@ -7,45 +6,53 @@ function App() {
     const [minutes, setMinutes] = useState(25);
     const [seconds, setSeconds] = useState(0);
     const [isActive, setIsActive] = useState(false);
+    const [isBreak, setBreak] = useState(false);
 
     useEffect(() => {
         let interval = null;
-        if (isActive && (minutes > 0 || seconds > 0)) {
+
+        if (isActive) {
             interval = setInterval(() => {
-                if (seconds === 0) {
+                if (seconds === 0 && minutes === 0) {
+
+                    if (!isBreak) {
+                        setMinutes(5);
+                        setSeconds(0);
+                        setBreak(true);
+                    } else {
+                        setIsActive(false);
+                    }
+                } else if (seconds === 0) {
                     setMinutes((prevMinutes) => prevMinutes - 1);
                     setSeconds(59);
                 } else {
                     setSeconds((prevSeconds) => prevSeconds - 1);
                 }
             }, 1000);
-        } else if (!isActive) {
-            clearInterval(interval);
         }
+
         return () => clearInterval(interval);
-    }, [isActive, seconds, minutes]);
+    }, [isActive, seconds, minutes, isBreak]);
 
     const resetTimer = () => {
         setIsActive(false);
         setMinutes(25);
         setSeconds(0);
+        setBreak(false);
     };
 
     const timerState = () => {
-        setIsActive(!isActive);
-    }
+        setIsActive((prev) => !prev);
+    };
 
-
-  return (
-      <div className="container">
-          <h1>POMODORO</h1>
-          <div className="clock">
-              <h1>{`${minutes}:${String(seconds).padStart(2, '0')}`}</h1> {/* Display time */}
-              <button onClick={timerState}>{isActive ? 'Pause' : 'Start'}</button>
-              <button onClick={resetTimer}>Reset</button>
-          </div>
-      </div>
-  )
+    return (
+        <div className={"container"}>
+            <h1>POMODORO</h1>
+            <h1>{`${minutes}:${String(seconds).padStart(2, '0')}`}</h1> {/* Display time */}
+            <button onClick={timerState}>{isActive ? 'Pause' : 'Start'}</button>
+            <button onClick={resetTimer}>Reset</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
