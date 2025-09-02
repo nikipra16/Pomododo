7bafabe56730a06019f65f50520af2bf4e8ae9fe
nikipra16 <142063028+nikipra16@users.noreply.github.com>
2025-09-02 13:24:46 -0700

    fixed refresh pages error

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { LinearProgress } from '@mui/material';
 import ToDo from './components/todo/todo.jsx';
 import theme from './components/theme.jsx';
 import { AppProvider, useAppContext } from './components/AppContext.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from "./components/signUp/signUp.jsx";
 import LogIn from './components/login/login.jsx';
 import Profile from './components/profile/profile.jsx';
@@ -39,7 +39,7 @@ function App() {
 export default function AppWrapper() {
     return (
         <AppProvider>
-            <Router basename="/Pomododo">
+            <Router>
                 <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/signup" element={<SignUp />} />
